7dce53d6071758cca2568371076a181be4a6fc65
phillipstemmlar <phillipstemmlar@gmail.com>
2020-05-16 16:50:34 +0200

    Automated commit with MakeFile

---
+++ 03/DFA.java
@@ -0,0 +1,168 @@
+import java.util.*;
+
+public class DFA{
+	DFAstate startState;
+
+	public DFA(DFAstate startState){
+		this.startState = startState;
+	}
+
+	public List<Token> evaluate(String input_str){
+		List<Token> tokens = new ArrayList<>();
+		List<String> strings = new ArrayList<>();
+
+		DFAstate curState = startState;
+		DFAstate prevState = null;
+
+		DFAstate savedState = null;
+		int savedIndex = -1;
+
+		int i = 0;
+		int err = 0;
+		int p = 0;
+		int startIndex = i;
+
+		String errorString = Lexer.defaultError;
+
+		while(i < input_str.length() && curState != null){
+			String c = input_str.substring(i,i+1);
+			prevState = curState;
+			curState = curState.transitions.get(c);
+			if(curState != null){
+				errorString = curState.getErrorString();
+			}
+			if(curState != null && curState.isFinal){
+				if(savedState == null){
+					savedState = curState;
+					savedIndex = i;
+				}
+				else if(((FinalDFAState)curState).isKeyword){
+					savedState = curState;
+					savedIndex = i;
+				}else if(savedState != null && !((FinalDFAState)savedState).isKeyword && !((FinalDFAState)curState).isKeyword){
+					savedState = curState;
+					savedIndex = i;
+				}
+			}
+			if(curState == null){
+				if(savedState != null){
+					Token.eToken token =  ((FinalDFAState)savedState).token;
+					String str = input_str.substring(startIndex,i);
+					err = p;
+					p = i;
+					tokens.add(new Token(token,str,Helper.getLineNumber(input_str,i), Helper.getColumnNumber(input_str, i)));
+					startIndex = i;
+					curState = startState;
+					i = savedIndex;
+					savedState = null;
+					savedIndex = -1;
+				}else{
+					i++;
+					String str = Helper.removeLeadingWhitespace(input_str.substring(err,i));
+					Helper.error(Lexer.PREFIX, Helper.getLineNumber(input_str,i), Helper.getColumnNumber(input_str,i), "\"" + str + "\" " + errorString);
+					return null;
+				}
+			}
+			i++;
+		}
+		if(curState != null && curState.isFinal){
+			Token.eToken token =  ((FinalDFAState)savedState).token;
+			String str = input_str.substring(startIndex,i);
+			tokens.add(new Token(token,str,Helper.getLineNumber(input_str,i), Helper.getColumnNumber(input_str, i)));
+		}else{
+			i++;
+			String str = Helper.removeLeadingWhitespace(input_str.substring(err,i));
+			Helper.error(Lexer.PREFIX, Helper.getLineNumber(input_str,i), Helper.getColumnNumber(input_str,i),  "\"" + str + "\" " + errorString);
+			return null;
+		}
+		List<Token> filter = new ArrayList<>();
+		for(int filt = 0; filt < tokens.size(); ++filt){
+			Token token = tokens.get(filt);
+			if(token.get() != Token.eToken.tok_space && token.get() != Token.eToken.tok_newline && token.get() != Token.eToken.tok_tab){
+				filter.add(token);
+			}
+		}
+		return filter;
+	}
+
+	public void addKeywordStates(String keyword, Token.eToken token){
+		final boolean isKeyword = true;
+		if(keyword.length() <= 1){
+			DFAstate state = new FinalDFAState(keyword,token,isKeyword);
+			state.setErrorString(Lexer.defaultError);	
+			startState.transitions.put(keyword, state);
+		}else{
+			DFAstate curState = null;
+			DFAstate prevState = startState;
+			for(int i = 0; i < keyword.length(); ++i){
+				if(prevState.transitions.containsKey(keyword.toCharArray()[i] + "")){
+					prevState = prevState.transitions.get(keyword.toCharArray()[i] + "");
+				}else{
+					if(i == keyword.length()-1){
+						curState = new FinalDFAState(keyword.toCharArray()[i] + "" + i, token,isKeyword);
+					}else{
+						curState = new NormalDFAState(keyword.toCharArray()[i] + "" + i);
+					}
+					curState.setErrorString(Lexer.defaultError);
+					prevState.transitions.put(keyword.toCharArray()[i]+"", curState);
+					prevState = curState;
+				}
+			}
+		}
+	}
+
+	public void addKeywordStates(String keyword, Token.eToken token,DFAstate nextState, Token.eToken intermediateToken){
+		final boolean isKeyword = true;
+		if(keyword.length() <= 1){
+			DFAstate fin = new FinalDFAState(keyword,token,isKeyword);
+			for(char i = 'a'; i <= 'z' && nextState != null; ++i) fin.addTransition(i+"", nextState);
+			for(int i = 1; i <= 9 && nextState != null; ++i) fin.addTransition(i+"", nextState);
+			startState.transitions.put(keyword, fin);
+		}else{
+			DFAstate curState = null;
+			DFAstate prevState = startState;
+			for(int i = 0; i < keyword.length(); ++i){
+				if(prevState.transitions.containsKey(keyword.toCharArray()[i] + "")){
+					DFAstate temp = prevState.transitions.get(keyword.toCharArray()[i] + "");
+					if(temp.isFinal && temp == nextState ){
+						if(i == keyword.length()-1){
+							curState = new FinalDFAState(keyword.toCharArray()[i] + "" + i, token,isKeyword);
+							prevState.transitions.put(keyword.toCharArray()[i]+"", curState);
+						}else{
+							curState = new FinalDFAState(keyword.toCharArray()[i] + "" + i, intermediateToken);			
+							
+							for(char c = 'a'; c <= 'z'; ++c){
+								if( ! curState.transitions.containsKey(c+"")) curState.addTransition(c + "", nextState);
+							}
+							// for(int c = 0; c <= 9 && prevState != startState; ++c) curState.addTransition(c + "", nextState);	
+							for(int c = 0; c <= 9; ++c) curState.addTransition(c + "", nextState);	
+						}
+						prevState.transitions.put(keyword.toCharArray()[i] + "", curState);
+						prevState = curState;
+					}else{
+						prevState = temp;
+					}
+				}else{
+					if(i == keyword.length()-1){
+						curState = new FinalDFAState(keyword.toCharArray()[i] + "" + i, token,isKeyword);
+						// prevState.transitions.put(keyword.toCharArray()[i]+"", curState);
+					}else{
+						curState = new FinalDFAState(keyword.toCharArray()[i] + "" + i, intermediateToken);
+					}	
+					prevState.transitions.put(keyword.toCharArray()[i]+"", curState);
+					for(char c = 'a'; c <= 'z'; ++c){
+						if( ! prevState.transitions.containsKey(c+"")) prevState.addTransition(c + "", nextState);
+					}
+					for(int c = 0; c <= 9 && prevState != startState; ++c) prevState.addTransition(c + "", nextState);		
+					prevState = curState;
+				}
+			}
+		}
+
+	}
+
+	public void print(){
+		startState.printTransitionTable();
+	}
+
+}
---
+++ 03/SemanticAnalyzer.java
@@ -0,0 +1,388 @@
+import java.util.*;
+
+public class SemanticAnalyzer {
+	private static String scope_PREFIX;
+	private static String type_PREFIX;
+	private static String value_PREFIX;
+
+	HashMap<Integer, Scope> scopes = null;
+
+	Boolean ERRORS = false;
+
+	public SemanticAnalyzer(String s_prefix, String t_prefix, String v_prefix){
+		scope_PREFIX = s_prefix;
+		type_PREFIX = t_prefix;
+		value_PREFIX = v_prefix;
+		scopes = new HashMap<>();
+	}
+
+	//=============================================Scope Checking=================================================
+	
+	public HashMap<Integer, Scope> executeScope(HashMap<Integer, SyntaxNode> table){
+		if(table != null){
+			ERRORS = false;
+			extractScopes(table);
+			checkScopes(table);
+			return (ERRORS)? null : scopes;
+		}
+		return null;
+	}
+	public HashMap<Integer, Scope> executeScopeToFile(HashMap<Integer, SyntaxNode> table, String treeTable_file, String symbolTable_file, String vis_tree_file){
+		scopes = executeScope(table);
+		if(scopes != null){
+			String treeIndexString = "", treeSymbolString = "";
+			for(Integer key : table.keySet()){
+				SyntaxNode node = table.get(key);
+				treeSymbolString += node.index + ":" + node.name2() + "\n";
+				if(!node.isLeaf()){
+					CompositeSyntaxNode comp = (CompositeSyntaxNode)node;
+					String childIndexList = "";
+					for(SyntaxNode child : comp.children) childIndexList += "," + child.index;
+					treeIndexString += comp.index + ":" + childIndexList.substring(1) + "\n";
+				}
+			}
+			Helper.writeToFile(treeTable_file, treeIndexString);
+			Helper.writeToFile(symbolTable_file, treeSymbolString);
+			Helper.writeToFile(vis_tree_file, table.get(0).treeString());
+		}
+		return scopes;
+	}
+
+	public void extractScopes(HashMap<Integer, SyntaxNode> table){
+		extractScopes(table.get(0), 0, null);
+	}
+	public void extractScopes(SyntaxNode node, int scopeLevel, Scope parent){
+		// String tabs = "";
+		// for(int i = 0; i < scopeLevel; ++i) tabs += "\t";
+
+		if(node.token == Token.eToken.PROG){
+			// System.out.println(tabs + "======NEW SCOPE=====");
+			Scope newScope = new Scope(scopeLevel,node);
+			if(parent != null){
+				parent.addScope(newScope);
+				newScope.parent = parent;
+			}
+			scopes.put(newScope.ID(), newScope);
+			node.innerScopeID(newScope.ID());
+			parent = newScope;
+		}
+		else if(node.token == Token.eToken.DECL){
+			CompositeSyntaxNode DECL = (CompositeSyntaxNode)node;
+			CompositeSyntaxNode TYPE = (CompositeSyntaxNode)DECL.children.get(0);
+			CompositeSyntaxNode NAME = (CompositeSyntaxNode)DECL.children.get(1);
+			LeafSyntaxNode NAME_val  = (LeafSyntaxNode)NAME.children.get(0);
+			DECL.line(NAME_val.line());
+			DECL.col(NAME_val.col());
+			parent.DECL_Lines.add(node.index);
+		}
+		else if(node.token == Token.eToken.PROC){
+			CompositeSyntaxNode PROC = (CompositeSyntaxNode)node;
+			LeafSyntaxNode NAME_val  = (LeafSyntaxNode)PROC.children.get(1);
+			PROC.line(NAME_val.line());
+			PROC.col(NAME_val.col());
+			parent.PROC_Lines.add(node.index);
+		}
+		else if(node.token == Token.eToken.VAR) parent.VAR_Lines.add(node.index);
+		else if(node.token == Token.eToken.CALL) parent.CALL_Lines.add(node.index);
+
+		// System.out.println(tabs + node.name());
+
+		if(!node.isLeaf()){
+			for(SyntaxNode child : ((CompositeSyntaxNode)node).children){
+				if(child.token == Token.eToken.PROG) extractScopes(child, scopeLevel + 1, parent);
+				else extractScopes(child, scopeLevel, parent);
+			}
+		}
+	}
+
+	public void checkScopes(HashMap<Integer, SyntaxNode> table){
+		if(scopes != null) checkScopes(table, scopes.get(0));
+	}
+
+	public void checkScopes(HashMap<Integer, SyntaxNode> table, Scope scope){
+		if(scope != null){
+			checkPROC(table, scope);
+			checkDECL(table, scope);
+			checkCALL(table, scope);
+			ckeckVAR(table, scope);
+
+			for(Integer id: scope.sub_Scopes.keySet()){
+				checkScopes(table, scope.sub_Scopes.get(id));
+			}
+		}
+	}
+
+	private void checkPROC(HashMap<Integer, SyntaxNode> table, Scope scope){
+		if(scope != null){
+			for(Integer i : scope.PROC_Lines){
+				SyntaxNode node = table.get(i);
+
+				CompositeSyntaxNode PROC = (CompositeSyntaxNode)node;
+				LeafSyntaxNode NAME_val  = (LeafSyntaxNode)PROC.children.get(1);
+				String name = NAME_val.val();
+
+				Procedure findProc = scope.checkProcedure(name);
+
+				if(findProc == null){
+					Procedure proc = new Procedure(name, i, scope, scopes.get(PROC.children.get(2).innerScopeID()) );
+					scope.addProcedure(proc);
+					NAME_val.val(proc.name());
+					NAME_val.scope(proc.lvl());
+				}else{
+					NAME_val.val(Procedure.noName());		
+					String errorString = errorRedeclaredParentProcedure(name,findProc.declLine(table));
+					if(scope.ID() == findProc.scopeID()){
+						errorString = errorRedeclaredProcedure(name,findProc.declLine(table));
+					}
+					Helper.error(scope_PREFIX,PROC.line(),PROC.col(),errorString);
+					ERRORS = true;
+				}
+
+			}
+		}
+	}
+
+	private void checkDECL(HashMap<Integer, SyntaxNode> table, Scope scope){
+		if(scope != null){
+			for(Integer i : scope.DECL_Lines){
+				SyntaxNode node = table.get(i);
+
+				CompositeSyntaxNode DECL = (CompositeSyntaxNode)node;
+				CompositeSyntaxNode TYPE = (CompositeSyntaxNode)DECL.children.get(0);
+				CompositeSyntaxNode NAME = (CompositeSyntaxNode)DECL.children.get(1);
+				LeafSyntaxNode NAME_val  = (LeafSyntaxNode)NAME.children.get(0);
+				String name = NAME_val.val();
+
+				Variable findVar = scope.findVariable(name);
+
+				if(findVar == null || findVar.lvl() != scope.lvl()){
+					Variable var = new Variable(Helper.tokenToType(TYPE.children.get(0).token),  name, i, scope.lvl());
+					scope.addVariable(var);
+					NAME_val.val(var.name());
+					NAME_val.scope(var.lvl());
+				}else{
+					NAME_val.val(Variable.noName());
+					Helper.error(scope_PREFIX,DECL.line(),DECL.col(),errorRedeclaredInSameScope(name,findVar.declLine(table)));
+					ERRORS = true;
+				}
+			}
+			
+		}
+	}
+
+	private void checkCALL(HashMap<Integer, SyntaxNode> table, Scope scope){
+		if(scope != null){
+			for(Integer i : scope.CALL_Lines){
+				SyntaxNode node = table.get(i);
+
+				CompositeSyntaxNode CALL = (CompositeSyntaxNode)node;
+				LeafSyntaxNode PROC_name  = (LeafSyntaxNode)CALL.children.get(0);
+				String name = PROC_name.val();
+				Procedure proc = scope.findProcedure(name);
+				scope.addProcedure(proc);
+				
+				if(proc != null){
+					PROC_name.val(proc.name());
+					PROC_name.scope(proc.lvl());
+				 }
+				else PROC_name.val(Procedure.noName());
+			}
+		}
+	}
+
+	private void ckeckVAR(HashMap<Integer, SyntaxNode> table, Scope scope){
+		if(scope != null){
+			for(Integer i : scope.VAR_Lines){
+				SyntaxNode node = table.get(i);
+
+				CompositeSyntaxNode VAR = (CompositeSyntaxNode)node;
+				LeafSyntaxNode VAR_name  = (LeafSyntaxNode)VAR.children.get(0);
+				String name = VAR_name.val();
+				Variable var = scope.findVariable(name);
+
+				if(var != null && var.index <= i){
+					VAR_name.val(var.name());
+					scope.addVariable(var);
+					VAR_name.scope(var.scopeLevel);
+				}
+				else {
+					VAR_name.val(Variable.noName());
+					Helper.error(scope_PREFIX,VAR_name.line(),VAR_name.col(),errorUndefinedVariable(name));
+					ERRORS = true;
+				}
+			}
+		}
+	}
+
+	//=============================================Type Checking=================================================
+
+	public HashMap<Integer, Scope> executeType(HashMap<Integer, SyntaxNode> table){
+		if(table != null){
+			ERRORS = false;
+			extractTypes(table);
+			return (ERRORS)? null : scopes;
+		}
+		return null;
+	}
+	public HashMap<Integer, Scope> executeTypeToFile(HashMap<Integer, SyntaxNode> table, String treeTable_file, String symbolTable_file, String vis_tree_file){
+		scopes = executeType(table);
+		if(scopes != null){
+			String treeIndexString = "", treeSymbolString = "", treeString = table.get(0).treeString();
+			for(Integer key : table.keySet()){
+				SyntaxNode node = table.get(key);
+				treeSymbolString += node.index + ":" + node.name2() + "\n";
+				if(!node.isLeaf()){
+					CompositeSyntaxNode comp = (CompositeSyntaxNode)node;
+					String childIndexList = "";
+					for(SyntaxNode child : comp.children) childIndexList += "," + child.index;
+					treeIndexString += comp.index + ":" + childIndexList.substring(1) + "\n";
+				}
+			}
+			// System.out.println(treeString);
+			Helper.writeToFile(treeTable_file, treeIndexString);
+			Helper.writeToFile(symbolTable_file, treeSymbolString);
+			Helper.writeToFile(vis_tree_file, treeString);
+		}
+		return scopes;
+	}
+
+	private void extractTypes(HashMap<Integer, SyntaxNode> table){
+		if(scopes != null) extractTypes(table.get(0), scopes.get(0), true);
+	}
+
+	private void extractTypes(SyntaxNode node, Scope scope, Boolean first){
+		if(!first && node.token == Token.eToken.PROG){
+			scope = scope.getScopeByIndex(node.index);
+			if(scope == null) return;
+		}
+		
+		node.type = checkType(node,scope);
+
+		String tabs = ""; for(int i = 0; i < scope.lvl(); ++i) tabs += "\t";
+		System.out.println(tabs+node.name3());
+
+		if(!node.isLeaf()){
+			for(SyntaxNode child : ((CompositeSyntaxNode)node).children){
+				extractTypes(child, scope, false);
+			}
+		}
+
+	}
+
+	public Variable.Type checkType(SyntaxNode node, Scope scope){
+		if(node.type != Variable.Type.none) return node.type;
+
+		if(node.token == Token.eToken.tok_string_literal || node.token == Token.eToken.tok_integer_literal
+				|| node.token == Token.eToken.tok_T || node.token == Token.eToken.tok_F
+				|| node.token == Token.eToken.tok_string || node.token == Token.eToken.tok_num || node.token == Token.eToken.tok_bool){
+			return Helper.tokenToType(node.token);
+		}
+		else if(node.token == Token.eToken.PROC){
+			CompositeSyntaxNode PROC = (CompositeSyntaxNode)node;
+			LeafSyntaxNode proc  = (LeafSyntaxNode)PROC.children.get(0);
+			LeafSyntaxNode PROC_name  = (LeafSyntaxNode)PROC.children.get(1);
+			CompositeSyntaxNode prog  = (CompositeSyntaxNode)PROC.children.get(2);
+
+			proc.type = checkType(proc, scope);
+			PROC_name.type = Procedure.Type;
+			prog.type = checkType(prog, scope);
+			return Variable.notype();
+		}
+		else if(node.token == Token.eToken.DECL){
+			CompositeSyntaxNode DECL = (CompositeSyntaxNode)node;
+			CompositeSyntaxNode TYPE = (CompositeSyntaxNode)DECL.children.get(0);
+			CompositeSyntaxNode NAME = (CompositeSyntaxNode)DECL.children.get(1);	
+			LeafSyntaxNode NAME_val  = (LeafSyntaxNode)NAME.children.get(0);
+			TYPE.type = checkType(TYPE,scope);
+			NAME.type = checkType(NAME,scope);
+			return Variable.notype();
+		}
+		else if(node.token == Token.eToken.TYPE){
+			CompositeSyntaxNode TYPE = (CompositeSyntaxNode)node;
+			LeafSyntaxNode TYPE_val  = (LeafSyntaxNode)TYPE.children.get(0);
+			TYPE_val.type = checkType(TYPE_val,scope);
+			return TYPE_val.type;
+		}
+		else if(node.token == Token.eToken.NAME){
+			CompositeSyntaxNode NAME = (CompositeSyntaxNode)node;	
+			LeafSyntaxNode NAME_val  = (LeafSyntaxNode)NAME.children.get(0);		
+			Variable var = scope.findVariableByName(NAME_val.val());
+			Variable.Type type = (var == null)? Variable.undefined() : var.type;
+			NAME_val.type = type;
+			return type;
+		}
+		else if(node.token == Token.eToken.IO){
+			CompositeSyntaxNode IO = (CompositeSyntaxNode)node;
+			LeafSyntaxNode io = (LeafSyntaxNode)IO.children.get(0);
+			CompositeSyntaxNode VAR = (CompositeSyntaxNode)IO.children.get(1);	
+
+			io.type = checkType(io,scope);
+			VAR.type = checkType(VAR,scope);
+			if(VAR.type != Variable.Type.string && VAR.type != Variable.Type.num && VAR.type != Variable.Type.bool){
+				Helper.error(type_PREFIX, VAR.line(), VAR.col(), errorInvalidIOtype(io.token,VAR.type));
+				ERRORS = true;
+			}
+			return Variable.notype();
+
+		}
+		else if(node.token == Token.eToken.VAR){
+			CompositeSyntaxNode VAR = (CompositeSyntaxNode)node;
+			LeafSyntaxNode VAR_val  = (LeafSyntaxNode)VAR.children.get(0);
+			Variable var = scope.findVariableByName(VAR_val.val());
+			Variable.Type type = (var == null)? Variable.undefined() : var.type;
+			VAR_val.type = type;
+			return type;
+		}
+		else if(node.token == Token.eToken.CALL){
+			CompositeSyntaxNode CALL = (CompositeSyntaxNode)node;
+			LeafSyntaxNode PROC_name  = (LeafSyntaxNode)CALL.children.get(0);
+			Procedure proc = scope.findProcedureByName(PROC_name.val());
+			PROC_name.type = (proc == null)? Variable.undefined() : Procedure.Type;
+			return Variable.notype();
+		}
+		
+
+
+		return Variable.notype();
+	}
+
+
+	//=============================================Additional Methods=================================================
+
+	private String errorRedeclaredInSameScope(String name, Integer declLine){
+		return "Variable \""+name+"\" has already been declared on line: "+declLine+" "
+		+"\n\tMultiple variables with the same name cannot be declared in the same scope.";
+	}
+
+	private String errorUndefinedVariable(String name){
+		return "Assigning a value to an undefined variable \""+name+"\"";
+	}
+
+	private String errorRedeclaredProcedure(String name, Integer declLine){
+		return "Procedure \""+name+"\" has already been declared in this scope on line: "+Helper.white+declLine +Helper.red;
+	}
+
+	private String errorRedeclaredParentProcedure(String name, Integer declLine){
+		return "Procedure \""+name+"\" has already been declared in an outer scope on line: "+Helper.white+declLine +Helper.red+ ""
+		+ "\n\tProcedures cannot have the same name as an ancestor procedure that wraps around it";
+	}
+
+	private String errorInvalidIOtype(Token.eToken token, Variable.Type type){
+		return "Invalid argument of type \""+type+"\" for " + Helper.tokenStr(token) + " operation."
+		+ "\n\tExpected an expression of type string, num or bool.";
+	}
+
+	public Scope getScope(Integer scopeID){
+		if(scopes != null) return scopes.get(scopeID);
+		return null;
+	}
+
+	public void printScopes(){
+		if(scopes != null){
+			for(Integer key : scopes.keySet()) scopes.get(key).print();
+		}else{
+			System.out.println("scopes is null");
+		}
+	}
+
+}
